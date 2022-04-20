42fac008d261c1c6fa9aa97163901c97a8d8848b
aa <atrinh@pm.me>
2022-04-20 11:28:11 +0200

    fixed for build

--- src/pages/Admin/EditPostPage.jsx
+++ src/pages/Admin/EditPostPage.jsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
 import AdminPostForm from "../../components/Admin/AdminPostForm";
 
-function EditPostPage() {
-  const [loadedPost, setLoadedPost] = useState({
-    author: "Alexander",
-    title: "My awesome Post",
-    content: "Super amazing, thanks for that!",
-    thumbnailLink:
-      "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg",
-  });
+const loadedPost = {
+  author: "Alexander",
+  title: "My awesome Post",
+  content: "Super amazing, thanks for that!",
+  thumbnailLink:
+    "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg",
+};
 
+function EditPostPage() {
   return (
     <div>
       <section className='update-form'>
