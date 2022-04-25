681eb5b15c048b7ed3298e349f197d756fcf9771
aa <atrinh@pm.me>
2022-04-25 16:44:39 +0200

    fixed tests

--- src/firebase.js
+++ src/firebase.js
@@ -18,7 +18,7 @@ import { getFirestore, collection, addDoc } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
+  apiKey: process.env.REACT_APP_APIKEY || 'mock-key',
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
   projectId: process.env.REACT_APP_PROJECTID,
   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
